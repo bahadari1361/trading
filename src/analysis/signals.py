bb38e2d8e0445c70708cc94bd03d9b666d88e75e
bahadari1361 <bahadarih@gmail.com>
2025-06-01 19:06:27 +0200

    Update signals.py

--- src/analysis/signals.py
+++ src/analysis/signals.py
@@ -1,3 +1,16 @@
-def generate_signal(data):
-    sma = calculate_sma(data)
-    return "BUY" if data['close'][-1] > sma[-1] else "SELL"
+from .indicators import calculate_sma, calculate_rsi
+
+def generate_signals(data):
+    """Basic SMA crossover strategy"""
+    signals = []
+    sma_short = calculate_sma(data, window=5)
+    sma_long = calculate_sma(data, window=20)
+    
+    for i in range(1, len(data)):
+        if sma_short[i] > sma_long[i] and sma_short[i-1] <= sma_long[i-1]:
+            signals.append("BUY")
+        elif sma_short[i] < sma_long[i] and sma_short[i-1] >= sma_long[i-1]:
+            signals.append("SELL")
+        else:
+            signals.append("HOLD")
+    return signals
